8a0edace0739aa458f0fe0bcd85d1889daec21d0
santhosh <santhoshrock251@gmail.com>
2021-12-29 10:47:11 +0530

    First Commit

---
+++ src/main/java/GitDemo/DemoGit.java
@@ -0,0 +1,18 @@
+package GitDemo;
+
+public class DemoGit {
+        final void run()
+        {
+            System.out.println("running..");
+        }
+        class Honda extends DemoGit
+        {
+            public static void main(String[] args)
+            {
+                DemoGit obj = new DemoGit();
+                obj.run();
+            }
+        }
+
+    }
+
